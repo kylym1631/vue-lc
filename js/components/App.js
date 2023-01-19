af0bcac00c60c4d4458fd498a382f00e92f769d0
kylym1631 <67018582+kylym1631@users.noreply.github.com>
2023-01-19 01:31:18 +0100

    7 bring it all together

--- js/components/App.js
+++ js/components/App.js
@@ -1,6 +1,7 @@
-import AppButton from "./AppButton.js";
-export default{
-    components:{
-        'app-button':AppButton
-    }
+import Assignments from "./Assignments.js"
+export default {
+    components:{Assignments},
+   template:`
+    <assignments ></assignments>
+   `
 }
