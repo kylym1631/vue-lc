05c43d5ed649edc516587112a4b0aa064f1007fe
kylym1631 <67018582+kylym1631@users.noreply.github.com>
2023-01-19 15:01:36 +0100

    8 Handle a Form Submission

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -2,9 +2,9 @@ import Assignment from "./Assignment.js"
 export default{
     components:{Assignment},
     template:`
-            <section v-show="assignments.length">
+        <section v-show="assignments.length">
             <h2 class="font-bold mb-2">{{title}}</h2>
-            <ul>
+            <ul class="border border-gray-600 divide-y divide-gray-600">
                 <assignment
                     v-for="assignment in assignments"
                     :key="assignment.id"
--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -5,6 +5,13 @@ export default{
       <section class="space-y-6">
         <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
         <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+
+        <form @submit.prevent="add">
+          <div class="border border-gray-600 text-black">
+          <input v-model="newAssignment" placeholder ="New assingment..." class="p-2"/>
+          <button type="submit" class="bg-white p-2 border-l">Add</submit>
+          </div>
+        </form>
       </section>
     
     `, 
@@ -14,7 +21,8 @@ export default{
                 { name:'Finish project', complete:false, id:1},
                 { name:'Read Chapter 4', complete:false, id:2},
                 { name:'Turn in homework', complete:false, id:3},
-            ]
+            ],
+            newAssignment:''
         }
     },
     computed:{
@@ -23,6 +31,16 @@ export default{
                 inProgress:this.assignments.filter(a=>!a.complete),
                 completed:this.assignments.filter(a=>a.complete),
             }
+        }
+    },
+    methods:{
+        add(){
+            this.assignments.push({
+                name:this.newAssignment,
+                completed:false,
+                id: this.assignments.length+1
+            });
+            this.newAssignment = '';
         }
     }
 }
