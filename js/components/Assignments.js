1975a32b2f97328513a7c771079ce2a28c16ca02
kylym1631 <67018582+kylym1631@users.noreply.github.com>
2023-01-21 18:06:52 +0100

    Lifecycle Hooks, Fake APIs, and AJAX

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -13,9 +13,6 @@ export default{
     data(){
         return{
             assignments:[
-                { name:'Finish project', complete:false, id:1, tag:'math'},
-                { name:'Read Chapter 4', complete:false, id:2, tag:'science'},
-                { name:'Turn in homework', complete:false, id:3,tag:'math'},
             ],
         }
     },
@@ -26,6 +23,13 @@ export default{
                 completed:this.assignments.filter(a=>a.complete),
             }
         }
+    },
+    created(){
+        fetch('http://localhost:3000/assignments')
+        .then(response =>response.json())
+        .then(assignments => {
+            this.assignments = assignments;
+        })
     },
     methods:{
         add(name){
